48de66556aab5490795ec6abf30062d2f3cd9e5b
churihyung <dlwk0807@gmail.com>
2022-02-15 20:37:20 +0900

    useEffect and css modulization

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
+import styles from "./App.module.css";
+import Button from "./Button";
+
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const upCount = () => setValue((cur) => cur + 1);
+  const changeKeyValue = (event) => setKeyword(event.target.value);
+  console.log("i can do this all day");
+  useEffect(() => console.log("i render only once"), []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log(`i search a movie : ${keyword}`);
+    }
+  }, [keyword]);
   return (
     <div>
-      <h1>Hello!!!</h1>
+      <input onChange={changeKeyValue} value={keyword}></input>
+      <h1 className={styles.title}>{counter}</h1>
+      <Button onClick={upCount} text={"123"} />
     </div>
   );
 }
