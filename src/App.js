c0c9f12cce71eed6dc8075825ea3c4f3e0d01fed
churihyung <dlwk0807@gmail.com>
2022-02-16 22:43:17 +0900

    clean up js file ( App to CoinList)

--- src/App.js
+++ src/App.js
@@ -1,35 +1,7 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []);
-  return (
-    <div>
-      <h1>The Coins Top10</h1>
-      <h2>{loading ? "Loading..." : null}</h2>
-      <ul>
-        {coins
-          .filter((coin) => coin.rank <= 50 && coin.rank > 0)
-          .map((coin, index) => {
-            return (
-              <li key={index}>
-                {coin.name}({coin.symbol}) price:${coin.quotes.USD.price}
-                (USD)
-              </li>
-            );
-          })}
-      </ul>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
---
+++ src/CoinList.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+
+function CoinList() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+  return (
+    <div>
+      <h1>The Coins Top10</h1>
+      <h2>{loading ? "Loading..." : null}</h2>
+      <ul>
+        {coins
+          .filter((coin) => coin.rank <= 50 && coin.rank > 0)
+          .map((coin, index) => {
+            return (
+              <li key={index}>
+                {coin.name}({coin.symbol}) price:${coin.quotes.USD.price}
+                (USD)
+              </li>
+            );
+          })}
+      </ul>
+    </div>
+  );
+}
+
+export default CoinList;
