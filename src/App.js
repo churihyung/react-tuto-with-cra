3f4f577bba09de564730121030af63f720d52299
churihyung <dlwk0807@gmail.com>
2022-02-16 21:43:47 +0900

    todo list used state(array)
    and use three dots in array
    "[1,2, ...[3,4,5]  = [1,2,3,4,5]"

--- src/App.js
+++ src/App.js
@@ -1,41 +1,39 @@
-import { useEffect, useState } from "react";
-import styles from "./App.module.css";
-import Button from "./Button";
-
-function Hello() {
-  useEffect(() => {
-    console.log("SHOW X)");
-    //cleanup 컴포넌트 사라질 때 실행되는 함수
-    return () => console.log("DESTROYED :(");
-  }, []);
-  return <h3>Hello!</h3>;
-}
+import { useState } from "react";
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const [showing, setShowing] = useState(false);
+  const [toDo, setTodo] = useState("");
+  const [toDos, setTodos] = useState([]);
 
-  const upCount = () => setValue((cur) => cur + 1);
-  const changeKeyValue = (event) => setKeyword(event.target.value);
-  const showHello = () => setShowing((prev) => !prev);
+  const onChange = (event) => setTodo(event.target.value);
 
-  console.log("i can do this all day");
-
-  useEffect(() => console.log("i render only once"), []);
-  useEffect(() => {
-    if (keyword !== "" && keyword.length > 5) {
-      console.log(`i search a movie : ${keyword}`);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
     }
-  }, [keyword]);
+    setTodos((curArray) => [...toDos, toDo]);
+    setTodo("");
 
+    console.log(toDos);
+  };
   return (
     <div>
-      <input onChange={changeKeyValue} value={keyword}></input>
-      <h1 className={styles.title}>{counter}</h1>
-      <Button onClick={upCount} text={"123"} />
-      <button onClick={showHello}>{showing ? "HIDE" : "SHOW"}</button>
-      {showing ? <Hello /> : null}
+      <h2>My To Do({toDos.length})</h2>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          type="text"
+          placeholder="write your to do"
+          value={toDo}
+        ></input>
+        <button>Add to Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
